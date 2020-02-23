f4d90ada559745c4888b09015769d892da26b1e9
UltraFaceguy <kattreinkyle@gmail.com>
2020-02-22 21:24:39 -0500

    Removing plugin specific message to players

--- src/main/java/com/tealcube/minecraft/bukkit/facecore/profile/PlayerJoinListener.java
+++ src/main/java/com/tealcube/minecraft/bukkit/facecore/profile/PlayerJoinListener.java
@@ -23,9 +23,6 @@
 package com.tealcube.minecraft.bukkit.facecore.profile;
 
 import com.google.common.base.Optional;
-import com.tealcube.minecraft.bukkit.facecore.FacecorePlugin;
-import com.tealcube.minecraft.bukkit.facecore.utilities.MessageUtils;
-import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -33,28 +30,16 @@ import org.bukkit.event.player.PlayerJoinEvent;
 
 public final class PlayerJoinListener implements Listener {
 
-    private final FacecorePlugin plugin;
-
-    public PlayerJoinListener(FacecorePlugin plugin) {
-        this.plugin = plugin;
+  @EventHandler(priority = EventPriority.MONITOR)
+  public void onPlayerJoinEvent(final PlayerJoinEvent event) {
+    Optional<Profile> profileOptional = PlayerResolver.getInstance()
+        .findProfile(event.getPlayer().getUniqueId());
+    Profile p = profileOptional.or(new Profile(event.getPlayer().getUniqueId(), event.getPlayer().getName()));
+    if (!p.getName().equals(event.getPlayer().getName())) {
+      p.setLastKnownName(p.getName());
+      p.setName(event.getPlayer().getName());
     }
-
-    @EventHandler(priority = EventPriority.MONITOR)
-    public void onPlayerJoinEvent(final PlayerJoinEvent event) {
-        Optional<Profile> profileOptional = PlayerResolver.getInstance().findProfile(event.getPlayer().getUniqueId());
-        Profile p = profileOptional.or(new Profile(event.getPlayer().getUniqueId(), event.getPlayer().getName()));
-        if (!p.getName().equals(event.getPlayer().getName())) {
-            p.setLastKnownName(p.getName());
-            p.setName(event.getPlayer().getName());
-            Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
-                @Override
-                public void run() {
-                    MessageUtils.sendMessage(event.getPlayer(),
-                            "<yellow>You recently did a name change! You must access your chests before you get another name change or you won't be able to unlock them.");
-                }
-            }, 20L * 3);
-        }
-        PlayerResolver.getInstance().addProfile(p);
-    }
+    PlayerResolver.getInstance().addProfile(p);
+  }
 
 }
