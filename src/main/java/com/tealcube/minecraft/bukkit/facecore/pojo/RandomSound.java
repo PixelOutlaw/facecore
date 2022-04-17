64a34b8d9eb76a0cd57cf4ae805431830e7cbc85
Faceguy <kattreinkyle@gmail.com>
2022-04-17 12:37:12 -0400

    Add license and sound category to RandomSound

--- src/main/java/com/tealcube/minecraft/bukkit/facecore/pojo/RandomSound.java
+++ src/main/java/com/tealcube/minecraft/bukkit/facecore/pojo/RandomSound.java
@@ -1,8 +1,32 @@
+/**
+ * The MIT License
+ * Copyright (c) 2015 Teal Cube Games
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package com.tealcube.minecraft.bukkit.facecore.pojo;
 
 import lombok.Data;
+import org.apache.commons.lang3.StringUtils;
 import org.bukkit.Location;
 import org.bukkit.Sound;
+import org.bukkit.SoundCategory;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 
@@ -13,8 +37,11 @@ public class RandomSound {
   private float pitchBase;
   private float volume;
   private final Sound sound;
+  private final SoundCategory soundCategory;
 
-  public RandomSound(Sound sound, float volume, float pitchBase, float pitchRange) {
+  public RandomSound(SoundCategory soundCategory, Sound sound, float volume,
+      float pitchBase, float pitchRange) {
+    this.soundCategory = soundCategory;
     this.sound = sound;
     this.volume = volume;
     this.pitchBase = pitchBase;
@@ -26,10 +53,18 @@ public class RandomSound {
     Sound sound = Sound.ENTITY_CHICKEN_DEATH;
     try {
       sound = Sound.valueOf(soundString);
-    } catch (Exception ignored) {
-
+    } catch (Exception ignored) {}
+    String categoryString = section.getString("sound-category");
+    SoundCategory category = SoundCategory.MASTER;
+    if (StringUtils.isNotBlank(categoryString)) {
+      try {
+        category = SoundCategory.valueOf(categoryString);
+      } catch (Exception ignored) {
+      }
     }
+
     return new RandomSound(
+        category,
         sound,
         (float) section.getDouble("volume", 1),
         (float) section.getDouble("pitch-base", 0.9),
@@ -38,12 +73,12 @@ public class RandomSound {
   }
 
   public void play(Location location) {
-    location.getWorld().playSound(location, sound, volume, (float) (pitchBase + Math.random() * pitchRange));
+    location.getWorld().playSound(location, sound, soundCategory, volume, (float) (pitchBase + Math.random() * pitchRange));
   }
 
   public void play(Location location, Player... players) {
     for (Player p : players) {
-      p.playSound(location, sound, volume, (float) (pitchBase + Math.random() * pitchRange));
+      p.playSound(location, sound, soundCategory, volume, (float) (pitchBase + Math.random() * pitchRange));
     }
   }
 
